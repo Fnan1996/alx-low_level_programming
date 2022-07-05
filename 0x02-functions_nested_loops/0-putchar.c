cad283d5254f86138dd276ce22c03314f5c28fbf
Fnan1996 <fnantedros09@gmail>
2022-07-05 02:45:10 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,7 @@
 #include "main.h";
-int main (void){
+
+int main (void)
+{
     _putchar('_');
     _putchar('p');
     _putchar('u');
@@ -11,5 +13,4 @@ int main (void){
     _putchar('\n');
     
     return (0);
-
 }
