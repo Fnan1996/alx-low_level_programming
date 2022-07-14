f2498481a86f73338c981358d6334a2b0bbe9c49
Fnan1996 <fnantedros09@gmail>
2022-07-14 13:41:25 -0700

    task10-11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *s);
 void print_number(int n);
-int (void);
-char *rot13(char *s);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
