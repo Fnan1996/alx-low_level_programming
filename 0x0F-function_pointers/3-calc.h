8ff6a0666ffe2ac104cfb83ae33f6b10c6820a72
Fnan1996 <fnantedros09@gmail>
2022-08-03 11:25:23 -0700

    task

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,9 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct op - Struct op
  *
@@ -13,10 +16,13 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 int (*get_op_func(char *s))(int, int);
+
 #endif
