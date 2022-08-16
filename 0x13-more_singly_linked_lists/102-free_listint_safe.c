78c265d55695f78da447cd2e4f57c6ebaf88fedc
Fnan1996 <fnantedros09@gmail>
2022-08-16 06:59:02 -0700

    task

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,12 @@
 #include "lists.h"
-
+#include <stdio.h>
 /**
- * free_listint_safe - frees a list
- * @h: the pointer to the start of list to free
- * Return: size of free'd list (in nodes?)
- */
+  * free_listint_safe - define function
+  * @h:
+  * Return: what does it return?
+  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodeCount = 0;
-	listint_t *temp = NULL;
-
-	if (!(h && *h))
-		return (nodeCount);
-	while (*h)
-	{
-		nodeCount++;
-		if (*h > (*h)->next)
-		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-		}
-	}
-	*h = NULL;
-	return (nodeCount);
+	h++;
+	return (0);
 }
